154238bc3e98cba0b663636e25e09d7f1ea8c114
Unknown <arne.heil@student.hu.nl>
2017-09-28 12:09:08 +0200

    wip

---
+++ Util/Token.cs
@@ -0,0 +1,67 @@
+using System;
+using Newtonsoft.Json.Linq;
+
+namespace exampleWebAPI.Util
+{
+    public partial class Token
+    {
+        public Token(string asFormattedString)
+        {
+            var token = JObject.Parse(asFormattedString);
+
+            AccessToken = (string) token["access_token"];
+            TokenType = (string) token["token_type"];
+            UserName = (string) token["userName"];
+//
+//            var convertedDate = DateTime.Parse(token[".issued"].ToString());
+//            Console.WriteLine("Converted {0} to {1} time {2}",
+//                token[".issued"].ToString(),
+//                convertedDate.Kind.ToString(),
+//                convertedDate);
+//
+
+            ExpiresIn = (int) token["expires_in"];
+            Issued = DateTime.Parse(token[".issued"].ToString());
+            Expires = DateTime.Parse(token[".expires"].ToString());
+            ;
+        }
+
+        public Token()
+        {
+            AccessToken = ":( :(";
+            TokenType = "China";
+            ExpiresIn = 7198;
+            UserName = "arneTest";
+            Issued = DateTime.Now.AddDays(-1);
+            Expires = DateTime.Now.AddDays(-1);
+        }
+
+        public string AccessToken { get; }
+        public string TokenType { get; }
+        private int ExpiresIn { get; }
+        private string UserName { get; }
+        private DateTime Issued { get; }
+        private DateTime Expires { get; }
+
+        public override string ToString()
+        {
+            return "" + AccessToken + " "
+                   + TokenType + " "
+                   + ExpiresIn + " "
+                   + ExpiresIn + " "
+                   + UserName + " "
+                   + Issued + " "
+                   + Expires;
+        }
+
+        public bool IsValid()
+        {
+            return Expires > DateTime.Now;
+        }
+
+        public string GetTokenString()
+        {
+            return "Bearer " + AccessToken;
+        }
+    }
+}
